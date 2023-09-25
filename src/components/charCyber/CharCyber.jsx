c6f23eb75cf8495afc662380e24b351289ee35b7
Tony <122675474+Noidan@users.noreply.github.com>
2023-09-25 17:54:29 +0300

    cyber

--- src/components/charCyber/CharCyber.jsx
+++ src/components/charCyber/CharCyber.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const CharCyber = () => {
   return (
-    <div>CharCyber</div>
+    <div>
+      CharCyber
+      Implants
+      Head
+      Body
+      Left arm
+      Right arm
+      Left leg
+      Right leg
+
+    </div>
   )
 }
 
